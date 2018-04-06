b383d0319cd6a1000242722e430ec3346306072d
Martin Pianello <martin.pianello@claro.com.ar>
2018-04-06 14:46:52 -0300

    + Info de la API documentada con Swagger

--- src/main/java/com/esolutions/trainings/App.java
+++ src/main/java/com/esolutions/trainings/App.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -30,7 +33,14 @@ public class App {
 
 	@Bean
 	public Docket api() {
+		final ApiInfo apiInfo =new ApiInfoBuilder()
+				.title("ESolutions  Java Starters Challenge")
+				.description("Desafío de programación Java basado en dominio de \"Partido de las estrellas\"")
+				.version("1.0")
+				.contact(new Contact("ESolutions", "http://www.esolutions.com.ar", "rrhh@esolutions.com.ar"))
+				.build();
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.ant("/matches/*"))
